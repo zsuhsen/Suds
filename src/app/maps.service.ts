1786b2605561206993b711bd550998305bf99b81
mikecislak <mjcislak@gmail.com>
2019-07-16 17:53:55 -0500

    Newest Version of Suds

---
+++ src/app/maps.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+
+interface Location {
+
+  latitude: string;
+  longitude: string;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapsService {
+
+  constructor(private http: HttpClient) { }
+
+  getLocation() {
+
+    return this.http.get<Location>('http://api.ipapi.com/api/check?access_key=a079babeef2082dea937e226cf794f64')
+  }
+
+}
