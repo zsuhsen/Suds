40af1d247466f36cd5302b28ce3a6b8f9edc07a9
Zach Suhsen <B2298359@ben.edu>
2019-07-15 20:17:52 -0500

    updated shit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -24,10 +24,27 @@ export class ProfileComponent implements OnInit {
 
 
   ngOnInit(): void {
-
+    this.route.data.subscribe(routeData => {
+      const data = routeData.data;
+      if (data) {
+        this.user = data;
+        this.createForm(this.user.name);
+      }
+    });
   }
 
+  createForm(name) {
+    this.profileForm = this.fb.group({
+      name: [name, Validators.required ]
+    });
+  }
 
+  save(value) {
+    this.userService.updateCurrentUser(value)
+      .then(res => {
+        console.log(res);
+      }, err => console.log(err));
+  }
 
 
   logout() {
