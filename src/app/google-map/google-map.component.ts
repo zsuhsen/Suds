04a32d4f4bc09c0d797f1b7c11d4087155050da0
Zach Suhsen <B2298359@ben.edu>
2019-07-17 21:38:02 -0500

    washers on map

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MapsService} from "../maps.service";
+import {MapsService} from '../maps.service';
+import {UserServiceService} from '../services/user-service.service';
 
 @Component({
   selector: 'app-google-map',
@@ -9,21 +10,24 @@ import {MapsService} from "../maps.service";
 export class GoogleMapComponent implements OnInit {
 
 
-  lat: string = '';
-  lng: string = '';
+  lat: string;
+  lng: string;
+  locations: Array<any>;
 
-  constructor(private map: MapsService) { }
+  constructor(private map: MapsService,
+              private us: UserServiceService) { }
 
   ngOnInit() {
-
     this.map.getLocation().subscribe(data => {
-
       console.log(data);
       this.lat = data.latitude;
       this.lng = data.longitude;
 
 
-    })
+    });
+    this.us.getWashers().subscribe(results => {
+      this.locations = results;
+    });
 
   }
 
